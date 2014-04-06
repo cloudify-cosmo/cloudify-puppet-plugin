0e04897271670ae221fe0a8c8feb77be6d36deec
Ilya Sher <ilya.sher@coding-knight.com>
2014-04-06 19:38:55 +0300

    WIP: Puppet plugin -- run on start or where specific tags exist

--- cloudify_plugin_puppet/operations.py
+++ cloudify_plugin_puppet/operations.py
@@ -2,7 +2,9 @@ import copy
 
 from cloudify.decorators import operation as _operation
 
-from cloudify_plugin_puppet.manager import *
+from cloudify_plugin_puppet.manager import (PuppetParamsError,
+                                            PuppetManager,
+                                            PUPPET_TAG_RE)
 
 EXPECTED_OP_PREFIXES = (
     'cloudify.interfaces.lifecycle',
@@ -34,41 +36,48 @@ def operation(ctx, **kwargs):
 
     ops_tags = props.get('operations_tags')
     if ops_tags:
-        op_tags = ops_tags.get(op)
+        op_tags = ops_tags.get(op, [])
         if isinstance(op_tags, basestring):
             op_tags = [op_tags]
         if not isinstance(op_tags, list):
-            raise PuppetParamsError("Operation tags must be a list, not {0}".
-                format(op_tags))
+            raise PuppetParamsError(
+                "Operation tags must be a list, not {0}".format(op_tags))
         if op_tags:
             ctx.logger.info("Operation '{0}' -> tags {1}".format(
                 op, op_tags))
             tags += op_tags
         else:
-            ctx.logger.info("No tags specific to operation '{0}'".format(op))
+            ctx.logger.info("No tags specific to operation '{0}', skipping".
+                            format(op))
+            return
+    else:
+        # Only run on "start"
+        if op != 'start':
+            return
 
     mgr = PuppetManager(ctx)
     # print(tags)
     mgr.run(tags=tags)
 
 if __name__ == '__main__':
+    import datetime
     from cloudify.mocks import MockCloudifyContext
     ctx = MockCloudifyContext(
         node_name='node_name',
         node_id=datetime.datetime.utcnow().strftime('node_name_%Y%m%d_%H%M%S'),
         operation='cloudify.interfaces.lifecycle.create',
         properties={
-        'puppet_config': {
-            'add_operation_tag': True,
-            'operations_tags': {
-                'create': ['op_tag_create', 'tag2'],
-            },
-            'environment': 'e1',
-            'tags': ['a', 'b'],
-            'server': 'puppet',
-            'node_name_prefix': 'pfx-',
-            'node_name_suffix': '.puppet.example.com',
-        }
-    })
+            'puppet_config': {
+                'add_operation_tag': True,
+                'operations_tags': {
+                    'create': ['op_tag_create', 'tag2'],
+                },
+                'environment': 'e1',
+                'tags': ['a', 'b'],
+                'server': 'puppet',
+                'node_name_prefix': 'pfx-',
+                'node_name_suffix': '.puppet.example.com',
+            }
+        })
 
     operation(ctx)
