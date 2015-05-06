57be795b634b2a7df7a3aec0cd490153165d1e8b
opencm <limor@gigaspaces.com>
2015-05-06 21:35:45 +0300

    Bump version to 1.2

--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ import setuptools
 setuptools.setup(
     zip_safe=False,
     name='cloudify-puppet-plugin',
-    version='1.2rc1',
+    version='1.2',
     author='ilya',
     author_email='ilya.sher@coding-knight.com',
     packages=['puppet_plugin'],
     license='LICENSE',
     description='Cloudify Chef plugin',
     install_requires=[
-        'cloudify-plugins-common>=3.2rc1',
+        'cloudify-plugins-common>=3.2',
     ],
     package_data={
         'puppet_plugin': [
